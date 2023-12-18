7b41cc4238d8b20ffce1b99d2f6f048c5af0ec38
Javi Coronel <coronel.chaves.francisco.javier@iescamas.es>
2023-12-18 19:37:02 +0100

    Añadida la funcion de analizar el tablero en busca de barcos y las condicionales que muestran el numero de barcos restantes en cada tablero o que jugador ha ganado

--- hundirFlotaCarlos/src/hundirFlotaCarlos/TablerosJavi.java
+++ hundirFlotaCarlos/src/hundirFlotaCarlos/TablerosJavi.java
@@ -34,5 +34,56 @@ public class TablerosJavi {
 		}
 		
  	}
+	
+	public static void comprobacionBarcos (int tableroJ1[][] , int tableroJ2[][]) {
+		
+		int contadorBarcosJ1 = 0;
+		int contadorBarcosJ2 = 0;
+		
+		
+		//El programa analiza el tablero en busca de numeros 1, que son los barcos, para contar cuantos quedan
+		for(int filas = 0 ; filas < tableroJ1.length ; filas++) {
+			
+			for(int columnas = 0 ; columnas < tableroJ2.length ; columnas++) {
+				
+				if(tableroJ1[filas][columnas] == 1) {
+					contadorBarcosJ1++;
+					
+				}
+			}
+		}
+		
+		for(int filas = 0 ; filas < tableroJ2.length ; filas++) {
+			
+			for(int columnas = 0 ; columnas < tableroJ2.length ; filas++) {
+				
+				if(tableroJ2[filas][columnas] == 1) {
+					contadorBarcosJ2++;
+					
+				}
+				
+			}
+		}
+		//Aqui el programa lo que hace es, a partir del resultado de los contadores, decir el numero de barcos
+		//que quedan o decir que jugador ha ganado en funcion del numero de barcos que quedan.
+		if(contadorBarcosJ1 > 0) {
+			
+			System.out.println("Al jugador 1 le quedan " + contadorBarcosJ1 + " barcos.");
+			
+		}else {
+			
+			System.out.println("Ha ganado el Jugador 2");
+			
+		}
+			
+		if(contadorBarcosJ2 > 0) {
+			
+			System.out.println("Al jugador 2 le quedan " + contadorBarcosJ2 + " barcos.");
+			
+		}else {
+			
+			System.out.println("Ha ganado el Jugador 2");
+		}
+	}
 
 }
